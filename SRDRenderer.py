7665353aecd37a9e955d6fa1d01430c88fa42f82
Isaac Weaver <wisaac407@gmail.com>
2015-09-08 21:22:53 -0500

    Space out the groups in the stats file.
    Give two new lines instead of just one.

--- SRDRenderer.py
+++ SRDRenderer.py
@@ -90,7 +90,7 @@ class SRDRenderer:
         lastgroup = ''
         for hook in self._active_hooks:
             if hook.hook_group != lastgroup:
-                s += '\n %s:\n%s\n' % (self._registered_groups[hook.hook_group][0], '='*50)
+                s += '\n\n %s:\n%s\n' % (self._registered_groups[hook.hook_group][0], '='*50)
                 lastgroup = hook.hook_group
             s += template.format(name=hook.hook_label, data=hook.post_render())
             s += '\n'
